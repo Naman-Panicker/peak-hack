ac342db3da8626ac1ba61433b8f182e196813253
Naman Panicker <Naman-Panicker>
2024-07-07 16:08:39 +0530

    Peak-hack Grid Comp

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import { BentoGridThirdDemo } from './components/ui/bentogriditem'
+import './index.css'
+
+function App() {
+
+
+  return <div>
+    <BentoGridThirdDemo/>
+     
+  </div>
+}
+
+export default App
